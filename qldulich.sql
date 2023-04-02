4ee3d8de753f0a740ad63c1fb2b8a16ed8ec59f9
huanhoang0101 <huanhoang0101@gmail.com>
2023-04-03 02:45:16 +0700

    update model

--- qldulich.sql
+++ qldulich.sql
@@ -82,7 +82,7 @@ CREATE TABLE `auth_permission` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`),
   CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=53 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,6 +91,7 @@ CREATE TABLE `auth_permission` (
 
 LOCK TABLES `auth_permission` WRITE;
 /*!40000 ALTER TABLE `auth_permission` DISABLE KEYS */;
+INSERT INTO `auth_permission` VALUES (1,'Can add permission',1,'add_permission'),(2,'Can change permission',1,'change_permission'),(3,'Can delete permission',1,'delete_permission'),(4,'Can view permission',1,'view_permission'),(5,'Can add group',2,'add_group'),(6,'Can change group',2,'change_group'),(7,'Can delete group',2,'delete_group'),(8,'Can view group',2,'view_group'),(9,'Can add content type',3,'add_contenttype'),(10,'Can change content type',3,'change_contenttype'),(11,'Can delete content type',3,'delete_contenttype'),(12,'Can view content type',3,'view_contenttype'),(13,'Can add user',4,'add_user'),(14,'Can change user',4,'change_user'),(15,'Can delete user',4,'delete_user'),(16,'Can view user',4,'view_user'),(17,'Can add location',5,'add_location'),(18,'Can change location',5,'change_location'),(19,'Can delete location',5,'delete_location'),(20,'Can view location',5,'view_location'),(21,'Can add post',6,'add_post'),(22,'Can change post',6,'change_post'),(23,'Can delete post',6,'delete_post'),(24,'Can view post',6,'view_post'),(25,'Can add tour',7,'add_tour'),(26,'Can change tour',7,'change_tour'),(27,'Can delete tour',7,'delete_tour'),(28,'Can view tour',7,'view_tour'),(29,'Can add user tour',8,'add_usertour'),(30,'Can change user tour',8,'change_usertour'),(31,'Can delete user tour',8,'delete_usertour'),(32,'Can view user tour',8,'view_usertour'),(33,'Can add tour image',9,'add_tourimage'),(34,'Can change tour image',9,'change_tourimage'),(35,'Can delete tour image',9,'delete_tourimage'),(36,'Can view tour image',9,'view_tourimage'),(37,'Can add tour comment',10,'add_tourcomment'),(38,'Can change tour comment',10,'change_tourcomment'),(39,'Can delete tour comment',10,'delete_tourcomment'),(40,'Can view tour comment',10,'view_tourcomment'),(41,'Can add post comment',11,'add_postcomment'),(42,'Can change post comment',11,'change_postcomment'),(43,'Can delete post comment',11,'delete_postcomment'),(44,'Can view post comment',11,'view_postcomment'),(45,'Can add rating',12,'add_rating'),(46,'Can change rating',12,'change_rating'),(47,'Can delete rating',12,'delete_rating'),(48,'Can view rating',12,'view_rating'),(49,'Can add like comment',13,'add_likecomment'),(50,'Can change like comment',13,'change_likecomment'),(51,'Can delete like comment',13,'delete_likecomment'),(52,'Can view like comment',13,'view_likecomment');
 /*!40000 ALTER TABLE `auth_permission` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,7 +108,7 @@ CREATE TABLE `django_content_type` (
   `model` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,6 +117,7 @@ CREATE TABLE `django_content_type` (
 
 LOCK TABLES `django_content_type` WRITE;
 /*!40000 ALTER TABLE `django_content_type` DISABLE KEYS */;
+INSERT INTO `django_content_type` VALUES (2,'auth','group'),(1,'auth','permission'),(3,'contenttypes','contenttype'),(13,'tourism','likecomment'),(5,'tourism','location'),(6,'tourism','post'),(11,'tourism','postcomment'),(12,'tourism','rating'),(7,'tourism','tour'),(10,'tourism','tourcomment'),(9,'tourism','tourimage'),(4,'tourism','user'),(8,'tourism','usertour');
 /*!40000 ALTER TABLE `django_content_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -141,35 +143,40 @@ CREATE TABLE `django_migrations` (
 
 LOCK TABLES `django_migrations` WRITE;
 /*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
-INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2023-03-17 16:38:54.827774'),(2,'contenttypes','0002_remove_content_type_name','2023-03-17 16:38:54.916647'),(3,'auth','0001_initial','2023-03-17 16:38:55.131977'),(4,'auth','0002_alter_permission_name_max_length','2023-03-17 16:38:55.183400'),(5,'auth','0003_alter_user_email_max_length','2023-03-17 16:38:55.189731'),(6,'auth','0004_alter_user_username_opts','2023-03-17 16:38:55.196714'),(7,'auth','0005_alter_user_last_login_null','2023-03-17 16:38:55.204694'),(8,'auth','0006_require_contenttypes_0002','2023-03-17 16:38:55.207687'),(9,'auth','0007_alter_validators_add_error_messages','2023-03-17 16:38:55.216659'),(10,'auth','0008_alter_user_username_max_length','2023-03-17 16:38:55.222644'),(11,'auth','0009_alter_user_last_name_max_length','2023-03-17 16:38:55.229625'),(12,'auth','0010_alter_group_name_max_length','2023-03-17 16:38:55.242590'),(13,'auth','0011_update_proxy_permissions','2023-03-17 16:38:55.249570'),(14,'auth','0012_alter_user_first_name_max_length','2023-03-17 16:38:55.256576'),(15,'tourism','0001_initial','2023-03-17 16:38:55.622731');
+INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2023-04-02 19:42:26.359897'),(2,'contenttypes','0002_remove_content_type_name','2023-04-02 19:42:26.464526'),(3,'auth','0001_initial','2023-04-02 19:42:26.725476'),(4,'auth','0002_alter_permission_name_max_length','2023-04-02 19:42:26.785239'),(5,'auth','0003_alter_user_email_max_length','2023-04-02 19:42:26.791823'),(6,'auth','0004_alter_user_username_opts','2023-04-02 19:42:26.798818'),(7,'auth','0005_alter_user_last_login_null','2023-04-02 19:42:26.804764'),(8,'auth','0006_require_contenttypes_0002','2023-04-02 19:42:26.810112'),(9,'auth','0007_alter_validators_add_error_messages','2023-04-02 19:42:26.816096'),(10,'auth','0008_alter_user_username_max_length','2023-04-02 19:42:26.824077'),(11,'auth','0009_alter_user_last_name_max_length','2023-04-02 19:42:26.830061'),(12,'auth','0010_alter_group_name_max_length','2023-04-02 19:42:26.845020'),(13,'auth','0011_update_proxy_permissions','2023-04-02 19:42:26.852003'),(14,'auth','0012_alter_user_first_name_max_length','2023-04-02 19:42:26.858014'),(15,'tourism','0001_initial','2023-04-02 19:42:28.017560');
 /*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `tourism_like`
+-- Table structure for table `tourism_likecomment`
 --
 
-DROP TABLE IF EXISTS `tourism_like`;
+DROP TABLE IF EXISTS `tourism_likecomment`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tourism_like` (
+CREATE TABLE `tourism_likecomment` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `created_date` datetime(6) NOT NULL,
   `updated_date` datetime(6) NOT NULL,
   `active` tinyint(1) NOT NULL,
+  `liked` tinyint(1) NOT NULL,
   `post_id` bigint NOT NULL,
   `user_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tourism_likecomment_post_id_user_id_2a60b319_uniq` (`post_id`,`user_id`),
+  KEY `tourism_likecomment_user_id_02e506a2_fk_tourism_user_id` (`user_id`),
+  CONSTRAINT `tourism_likecomment_post_id_9b30d9bd_fk_tourism_post_id` FOREIGN KEY (`post_id`) REFERENCES `tourism_post` (`id`),
+  CONSTRAINT `tourism_likecomment_user_id_02e506a2_fk_tourism_user_id` FOREIGN KEY (`user_id`) REFERENCES `tourism_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tourism_like`
+-- Dumping data for table `tourism_likecomment`
 --
 
-LOCK TABLES `tourism_like` WRITE;
-/*!40000 ALTER TABLE `tourism_like` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tourism_like` ENABLE KEYS */;
+LOCK TABLES `tourism_likecomment` WRITE;
+/*!40000 ALTER TABLE `tourism_likecomment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tourism_likecomment` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -214,7 +221,9 @@ CREATE TABLE `tourism_post` (
   `content` longtext NOT NULL,
   `number_like` int NOT NULL,
   `user_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `tourism_post_user_id_59a5fe26_fk_tourism_user_id` (`user_id`),
+  CONSTRAINT `tourism_post_user_id_59a5fe26_fk_tourism_user_id` FOREIGN KEY (`user_id`) REFERENCES `tourism_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -242,7 +251,11 @@ CREATE TABLE `tourism_postcomment` (
   `content` varchar(255) NOT NULL,
   `post_id` bigint NOT NULL,
   `user_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `tourism_postcomment_post_id_ee4dea31_fk_tourism_post_id` (`post_id`),
+  KEY `tourism_postcomment_user_id_7c6b9cdd_fk_tourism_user_id` (`user_id`),
+  CONSTRAINT `tourism_postcomment_post_id_ee4dea31_fk_tourism_post_id` FOREIGN KEY (`post_id`) REFERENCES `tourism_post` (`id`),
+  CONSTRAINT `tourism_postcomment_user_id_7c6b9cdd_fk_tourism_user_id` FOREIGN KEY (`user_id`) REFERENCES `tourism_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -267,10 +280,14 @@ CREATE TABLE `tourism_rating` (
   `created_date` datetime(6) NOT NULL,
   `updated_date` datetime(6) NOT NULL,
   `active` tinyint(1) NOT NULL,
-  `value` int NOT NULL,
+  `value` smallint NOT NULL,
   `tour_id` bigint NOT NULL,
   `user_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `tourism_rating_tour_id_user_id_3e7b09a5_uniq` (`tour_id`,`user_id`),
+  KEY `tourism_rating_user_id_89598a8f_fk_tourism_user_id` (`user_id`),
+  CONSTRAINT `tourism_rating_tour_id_63252a1a_fk_tourism_tour_id` FOREIGN KEY (`tour_id`) REFERENCES `tourism_tour` (`id`),
+  CONSTRAINT `tourism_rating_user_id_89598a8f_fk_tourism_user_id` FOREIGN KEY (`user_id`) REFERENCES `tourism_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -281,32 +298,6 @@ CREATE TABLE `tourism_rating` (
 LOCK TABLES `tourism_rating` WRITE;
 /*!40000 ALTER TABLE `tourism_rating` DISABLE KEYS */;
 /*!40000 ALTER TABLE `tourism_rating` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tourism_role`
---
-
-DROP TABLE IF EXISTS `tourism_role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tourism_role` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `created_date` datetime(6) NOT NULL,
-  `updated_date` datetime(6) NOT NULL,
-  `active` tinyint(1) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tourism_role`
---
-
-LOCK TABLES `tourism_role` WRITE;
-/*!40000 ALTER TABLE `tourism_role` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tourism_role` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -324,7 +315,6 @@ CREATE TABLE `tourism_tour` (
   `name` varchar(255) NOT NULL,
   `duration` int NOT NULL,
   `description` longtext NOT NULL,
-  `image` varchar(100) DEFAULT NULL,
   `location_id` bigint NOT NULL,
   PRIMARY KEY (`id`),
   KEY `tourism_tour_location_id_c419b344_fk_tourism_location_id` (`location_id`),
@@ -356,7 +346,11 @@ CREATE TABLE `tourism_tourcomment` (
   `content` varchar(255) NOT NULL,
   `tour_id` bigint NOT NULL,
   `user_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `tourism_tourcomment_tour_id_b38db559_fk_tourism_tour_id` (`tour_id`),
+  KEY `tourism_tourcomment_user_id_cd888e95_fk_tourism_user_id` (`user_id`),
+  CONSTRAINT `tourism_tourcomment_tour_id_b38db559_fk_tourism_tour_id` FOREIGN KEY (`tour_id`) REFERENCES `tourism_tour` (`id`),
+  CONSTRAINT `tourism_tourcomment_user_id_cd888e95_fk_tourism_user_id` FOREIGN KEY (`user_id`) REFERENCES `tourism_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -383,7 +377,9 @@ CREATE TABLE `tourism_tourimage` (
   `active` tinyint(1) NOT NULL,
   `value` varchar(100) DEFAULT NULL,
   `tour_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `tourism_tourimage_tour_id_f60d50fb_fk_tourism_tour_id` (`tour_id`),
+  CONSTRAINT `tourism_tourimage_tour_id_f60d50fb_fk_tourism_tour_id` FOREIGN KEY (`tour_id`) REFERENCES `tourism_tour` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -505,7 +501,11 @@ CREATE TABLE `tourism_usertour` (
   `status` int NOT NULL,
   `tour_id` bigint NOT NULL,
   `user_id` bigint NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `tourism_usertour_tour_id_33a09a8b_fk_tourism_tour_id` (`tour_id`),
+  KEY `tourism_usertour_user_id_0cc2e016_fk_tourism_user_id` (`user_id`),
+  CONSTRAINT `tourism_usertour_tour_id_33a09a8b_fk_tourism_tour_id` FOREIGN KEY (`tour_id`) REFERENCES `tourism_tour` (`id`),
+  CONSTRAINT `tourism_usertour_user_id_0cc2e016_fk_tourism_user_id` FOREIGN KEY (`user_id`) REFERENCES `tourism_user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -527,4 +527,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-17 23:48:54
+-- Dump completed on 2023-04-03  2:44:13
--- tourism_management/tourism/models.py
+++ tourism_management/tourism/models.py
@@ -12,8 +12,8 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class Role(BaseModel):
-    name = models.CharField(max_length=50)
+# class Role(BaseModel):
+#    name = models.CharField(max_length=50)
 
 
 class User(AbstractUser):
@@ -22,6 +22,9 @@ class User(AbstractUser):
 
 class Location(BaseModel):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Tour(BaseModel):
@@ -32,8 +35,11 @@ class Tour(BaseModel):
     description = RichTextField()
     number_rate = models.FloatField
     max_person = models.IntegerField
-    image = models.ImageField(upload_to='courses/%Y/%m', null=True)
-    location = models.ForeignKey(Location, on_delete=models.PROTECT)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+#    user = models.ManyToManyField(User, through='UserTour')
+    def __str__(self):
+        return self.name
 
 
 class UserTour(BaseModel):
@@ -43,40 +49,56 @@ class UserTour(BaseModel):
     date_finish = models.DateTimeField()
     total_price = models.DecimalField
     status = models.IntegerField()
-    tour = models.ForeignKey(Tour, on_delete=models.PROTECT)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 class TourImage(BaseModel):
     value = models.ImageField(upload_to='users/%Y/%m', null=True)
-    tour = models.ForeignKey(Tour, on_delete=models.PROTECT)
+    tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
 
 
 class TourComment(BaseModel):
     content = models.CharField(max_length=255)
-    tour = models.ForeignKey(Tour, on_delete=models.PROTECT)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
 
 
 class Rating(BaseModel):
-    value = models.IntegerField()
-    tour = models.ForeignKey(Tour, on_delete=models.PROTECT)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    value = models.SmallIntegerField(default=0)
+    tour = models.ForeignKey(Tour, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('tour', 'user')
 
 
 class Post(BaseModel):
     title = models.CharField(max_length=255)
     content = RichTextField()
     number_like = models.IntegerField()
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 class PostComment(BaseModel):
     content = models.CharField(max_length=255)
-    post = models.ForeignKey(Post, on_delete=models.PROTECT)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
 
 
-class Like(BaseModel):
-    post = models.ForeignKey(Post, on_delete=models.PROTECT)
-    user = models.ForeignKey(User, on_delete=models.PROTECT)
+class LikeComment(BaseModel):
+    liked = models.BooleanField(default=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('post', 'user')
